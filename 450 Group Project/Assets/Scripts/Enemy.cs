b189291c2b7eeeb0c667ce76850b1ee7c3210780
Pierce <prh9765@rit.edu>
2021-02-26 14:06:45 -0500

    Added More Functionality

--- 450 Group Project/Assets/Scripts/Enemy.cs
+++ 450 Group Project/Assets/Scripts/Enemy.cs
@@ -9,11 +9,15 @@ public class Enemy:MonoBehaviour
     public bool isDead;
     public bool isMoving;
     public Transform trans;
+    public bool isColliding;
+    public GameManager man;
 
     // Start is called before the first frame update
     void Start()
     {
+        man = GameObject.Find("Game Manager").GetComponent<GameManager>();
         isDead = false;
+        isColliding = false;
         hp = 1;
     }
 
@@ -29,5 +33,14 @@ public class Enemy:MonoBehaviour
     void OnMouseDown()
     {
         TakeDamage();
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            Debug.Log("Hit");
+            man.TakeDamage();
+        }
     }
 }
--- 450 Group Project/Assets/Scripts/GameManager.cs
+++ 450 Group Project/Assets/Scripts/GameManager.cs
@@ -7,35 +7,71 @@ public class GameManager : MonoBehaviour
     public int wave;
     public List<GameObject> enemies;
     public GameObject enemy;
+    public int hp;
+    public bool isDead;
+    public bool waveRunning;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
+        hp = 1;
+        isDead = false;
         wave = 0;
-        enemies = new List<GameObject>();
-        enemies.Add(Instantiate(enemy));
-        enemies.Add(Instantiate(enemy, new Vector3(-8f,5f,0), Quaternion.identity));
-        enemies[0].GetComponent<Enemy>().isMoving = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        for(int i=0; i<enemies.Count; i++)
+        if(Input.GetKeyDown(KeyCode.Return) && !waveRunning)
         {
-            if(enemies[i].GetComponent<Enemy>().isDead)
+            waveRunning = true;
+            GenerateWave();
+        }
+        if (!isDead && waveRunning)
+        {
+            enemies[0].GetComponent<Enemy>().isMoving = true;
+            for (int i = 0; i < enemies.Count; i++)
             {
-                Destroy(enemies[i]);
-                enemies.RemoveAt(i);
-                continue;
-            }
+                if (enemies[i].GetComponent<Enemy>().isDead)
+                {
+                    if (enemies.Count == 1)
+                    {
+                        waveRunning = false;
+                    }
+                    Destroy(enemies[i]);
+                    enemies.RemoveAt(i);
+                    continue;
+                }
 
-            if (enemies[i].GetComponent<Enemy>().isMoving)
-            {
-                Vector2 temp = enemies[i].transform.position;
-                temp.x += .001f;
-                enemies[i].transform.position = temp;
+                if (enemies[i].GetComponent<Enemy>().isMoving)
+                {
+                    Vector2 temp = enemies[i].transform.position;
+                    temp.x += .001f;
+                    enemies[i].transform.position = temp;
+                }
             }
         }
+    }
+
+    public void TakeDamage()
+    {
+        hp--;
+        if (hp <= 0)
+        {
+            isDead = true;
+            Debug.Log("Game Over");
+        }
+    }
+
+    public void GenerateWave()
+    {
+        wave++;
+        enemies = new List<GameObject>();
+        for (int i = 0; i < wave; i++)
+        {
+            enemies.Add(Instantiate(enemy));
+        }
+        enemies.Add(Instantiate(enemy, new Vector3(-8f, 5f, 0), Quaternion.identity));
     }
 }
