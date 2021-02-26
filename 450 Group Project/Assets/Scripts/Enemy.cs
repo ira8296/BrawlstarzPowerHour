1f14bc1101f43cb902b0201c289d1d2c4f3f0d2a
Pierce <prh9765@rit.edu>
2021-02-26 13:32:28 -0500

    Added some more features

---
+++ 450 Group Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy:MonoBehaviour
+{
+    //Potentially change from public
+    public int hp;
+    public bool isDead;
+    public bool isMoving;
+    public Transform trans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDead = false;
+        hp = 1;
+    }
+
+    public void TakeDamage()
+    {
+        hp--;
+        if(hp<=0)
+        {
+            isDead = true;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        TakeDamage();
+    }
+}
---
+++ 450 Group Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int wave;
+    public List<GameObject> enemies;
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wave = 0;
+        enemies = new List<GameObject>();
+        enemies.Add(Instantiate(enemy));
+        enemies.Add(Instantiate(enemy, new Vector3(-8f,5f,0), Quaternion.identity));
+        enemies[0].GetComponent<Enemy>().isMoving = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i=0; i<enemies.Count; i++)
+        {
+            if(enemies[i].GetComponent<Enemy>().isDead)
+            {
+                Destroy(enemies[i]);
+                enemies.RemoveAt(i);
+                continue;
+            }
+
+            if (enemies[i].GetComponent<Enemy>().isMoving)
+            {
+                Vector2 temp = enemies[i].transform.position;
+                temp.x += .001f;
+                enemies[i].transform.position = temp;
+            }
+        }
+    }
+}
